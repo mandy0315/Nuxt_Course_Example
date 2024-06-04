a8370f515a25ea4307cbade66a130af9f1d660ad
mandy <mandy3152618@gmail.com>
2024-06-04 09:47:58 +0800

    add: CH5-嶔套式頁面練

---
+++ CH05/exercise/nuxt3-auto-router/pages/about.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>about</div>
+  <ul>
+    <li>
+      <NuxtLink to="/about">about</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about/a">a</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about/b">b</NuxtLink>
+    </li>
+  </ul>
+  <main>
+    <NuxtPage />
+  </main>
+</template>
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  list-style: none;
+
+  li {
+    margin: 0 10px;
+  }
+}
+</style>

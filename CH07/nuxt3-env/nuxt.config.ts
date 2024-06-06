85938fce67e0bd67d3ac9ad643acb09dd700876f
mandy <mandy3152618@gmail.com>
2024-06-06 16:59:13 +0800

    add: CH07 環境變數練習

--- CH07/nuxt3-env/nuxt.config.ts
+++ CH07/nuxt3-env/nuxt.config.ts
@@ -1,1 +1,14 @@
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // runtimeConfig: {
+  //   token: "",
+  //   public: {
+  //     apiUrl: "",
+  //   },
+  // },
+  vite: {
+    define: {
+      "process.env": process.env,
+    },
+  },
+});
--- CH07/nuxt3-env/pages/index.vue
+++ CH07/nuxt3-env/pages/index.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+const config = useRuntimeConfig();
+// console.log('apiUrl=>', config.public.apiUrl);
+// console.log('token=>', config.token);
+
+console.log("ENV=>", process.env.ENV);
+console.log("WEB_URL=>", process.env.WEB_URL);
+</script>
 
 <template>
   <div>
